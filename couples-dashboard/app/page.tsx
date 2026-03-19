d7dd59f61013c4448ae2166b54932105ecd176d3
teybearr <altheaquiatchon214@gmail.com>
2026-03-19 17:35:32 +0800

    fixed top 3

--- couples-dashboard/app/page.tsx
+++ couples-dashboard/app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { supabase } from "@/lib/supabase";
 
+export const revalidate = 0;
+
 const navItems = [
   {
     href: "/dates",
